b49b642e356e032cf69ccb2fa19b191eef2db079
Justin Karijo <justinkarijo21@gmail.com>
2026-05-27 00:10:48 +0200

    De bus trein knop is nu werkzaam. Er zijn verschillende tijden te zien bij bus en verschillende tijden bij trein.

--- src/main/java/com/example/demo/model/FixedRoutes.java
+++ src/main/java/com/example/demo/model/FixedRoutes.java
@@ -10,15 +10,15 @@ public class FixedRoutes {
                 new Trajectory("Zwolle", "Utrecht", List.of("08:30", "10:15", "17:35"), 55, TransportType.BUS),
                 new Trajectory("Utrecht", "Zwolle", List.of("09:05", "12:05", "18:10"), 55, TransportType.BUS),
                 //Trein routes
-                new Trajectory("Zwolle", "Utrecht", List.of("7:00", "11:30", "16:45"), 45, TransportType.TREIN),
-                new Trajectory("Utrecht", "Zwolle", List.of("8:15", "13:00", "17:20"), 45, TransportType.TREIN),
+                new Trajectory("Zwolle", "Utrecht", List.of("07:00", "11:30", "16:45"), 45, TransportType.TREIN),
+                new Trajectory("Utrecht", "Zwolle", List.of("08:15", "13:00", "17:20"), 45, TransportType.TREIN),
 
                 //Bus
                 new Trajectory("Amsterdam", "Rotterdam", List.of("07:05", "12:40", "18:20"), 95, TransportType.BUS),
                 new Trajectory("Rotterdam", "Amsterdam", List.of("08:10", "14:05", "19:40"), 95, TransportType.BUS),
                 //Trein
-                new Trajectory("Amsterdam", "Rotterdam", List.of("7:45", "13:00", "19:00"), 50, TransportType.TREIN),
-                new Trajectory("Rotterdam", "Amsterdam", List.of("9:00", "11:15", "20:00"), 50, TransportType.TREIN),
+                new Trajectory("Amsterdam", "Rotterdam", List.of("07:45", "13:00", "19:00"), 50, TransportType.TREIN),
+                new Trajectory("Rotterdam", "Amsterdam", List.of("09:00", "11:15", "20:00"), 50, TransportType.TREIN),
 
                 //Bus
                 new Trajectory("Groningen", "Leeuwarden", List.of("09:10", "13:55", "20:05"), 100,TransportType.BUS),
