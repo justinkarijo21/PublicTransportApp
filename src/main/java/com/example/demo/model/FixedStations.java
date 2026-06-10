6e8623ee7ac3f27f9caa305de07a529d4f8edafb
NurtenERD <nerdogan@outlook.com>
2026-06-10 10:43:52 +0200

    toevoegen Station en FixedStations

---
+++ src/main/java/com/example/demo/model/FixedStations.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class FixedStations {
+    public static List<Station> getAllStations() {
+        return List.of(
+                new Station("Amersfoort", 6, true, "Amersfoort"),
+                new Station("Zwolle", 8, true, "Zwolle"),
+                new Station("Amsterdam", 15, true, "Amsterdam"),
+                new Station("Rotterdam", 13, true, "Rotterdam"),
+                new Station("Groningen", 6, true, "Groningen"),
+                new Station("Leeuwarden", 4, true, "Leeuwarden"),
+                new Station("Den Haag", 12, true, "Den Haag"),
+                new Station("Eindhoven", 6, true, "Eindhoven"),
+                new Station("Maastricht", 5, false, "Maastricht"),
+                new Station("Nijmegen", 4, true, "Nijmegen"),
+                new Station("Utrecht", 12, true, "Utrecht" )
+        );
+    }
+}
